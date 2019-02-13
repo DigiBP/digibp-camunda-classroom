d6fabf517124cf092391daa3930dd7c1117be4e1
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-02-13 11:49:59 +0100

    Fix swagger

--- src/main/java/ch/fhnw/digibp/classroom/config/SwaggerConfig.java
+++ src/main/java/ch/fhnw/digibp/classroom/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/generator/**"))
+                .paths(PathSelectors.ant("/classroom/**"))
                 .build();
     }
 }
