b6277a429680cf6141802e7b06f581e54dd681c2
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-02-13 17:59:01 +0100

    Security

---
+++ src/main/java/ch/fhnw/digibp/classroom/config/CamundaSecurityFilter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.digibp.classroom.config;
+
+import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class CamundaSecurityFilter {
+
+    @Bean
+    public FilterRegistrationBean processEngineAuthenticationFilter() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setName("camunda-auth");
+        registration.setFilter(getProcessEngineAuthenticationFilter());
+        registration.addInitParameter("authentication-provider",
+                "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+        //registration.addUrlPatterns("/rest/*");
+        registration.addUrlPatterns("/classroom/*");
+        return registration;
+    }
+
+    @Bean
+    public Filter getProcessEngineAuthenticationFilter() {
+        return new ProcessEngineAuthenticationFilter();
+    }
+
+    @Bean
+    public Filter httpsEnforcerFilter(){
+        return new HttpsEnforcer();
+    }
+}
---
+++ src/main/java/ch/fhnw/digibp/classroom/config/HttpsEnforcer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.digibp.classroom.config;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HttpsEnforcer implements Filter {
+
+    private static final String X_FORWARDED_PROTO = "x-forwarded-proto";
+
+    @Override
+    public void init(FilterConfig filterConfig) {  }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (request.getHeader(X_FORWARDED_PROTO) != null) {
+            if (request.getHeader(X_FORWARDED_PROTO).indexOf("https") != 0) {
+                String pathInfo = (request.getPathInfo() != null) ? request.getPathInfo() : "";
+                response.sendRedirect("https://" + request.getServerName() + pathInfo);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() { }
+}
