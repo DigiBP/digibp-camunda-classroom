ac762ec42eb2f32cbda089605632966fdf712665
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-02-22 17:38:39 +0100

    Fix

--- src/main/resources/META-INF/resources/webjars/camunda/app/custom-ng-module/script.js
+++ src/main/resources/META-INF/resources/webjars/camunda/app/custom-ng-module/script.js
@@ -9,19 +9,25 @@ define('custom-ng-module', [
 ], function (angular) {
     var customModule = angular.module('classroom.redirect.module', []);
     customModule.controller('camHeaderViewsCtrl', ['$window', '$scope', function($window, $scope) {
-        var isLoggedIn = true;
-        var listener = $scope.$root.$on('authentication.changed', function(ev, auth) {
-            if(!auth && isLoggedIn) {
-                $window.location.href = $window.location.protocol + '//' + $window.location.hostname + ($window.location.port ? ':' + $window.location.port : '');
-                isLoggedIn = false;
-            }
-        });
-
-        $scope.$on('$destroy', function() {
-            listener();
-        });
-
+        redirect($window, $scope);
+    }]);
+    customModule.controller('NavigationController', ['$window', '$scope', function($window, $scope) {
+        redirect($window, $scope);
     }]);
 
     return customModule;
 });
+
+function redirect ($window, $scope) {
+    var isLoggedIn = true;
+    var listener = $scope.$root.$on('authentication.changed', function(ev, auth) {
+        if(!auth && isLoggedIn) {
+            $window.location.href = $window.location.protocol + '//' + $window.location.hostname + ($window.location.port ? ':' + $window.location.port : '');
+            isLoggedIn = false;
+        }
+    });
+
+    $scope.$on('$destroy', function() {
+        listener();
+    });
+}
