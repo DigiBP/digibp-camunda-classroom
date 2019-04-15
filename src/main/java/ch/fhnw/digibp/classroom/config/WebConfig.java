92ca8c8095ebbad95906e30e130691d4c9cf149f
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-04-15 10:22:11 +0200

    Enable CORS

---
+++ src/main/java/ch/fhnw/digibp/classroom/config/WebConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.digibp.classroom.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+                //.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS")
+                //.allowedOrigins("*")
+                //.allowCredentials(true);
+    }
+}
