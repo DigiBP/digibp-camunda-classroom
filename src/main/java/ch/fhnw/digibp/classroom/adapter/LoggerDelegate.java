44606d0ab9fe7737f3a6c3524b789ecc7af1828f
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-05-21 12:22:16 +0200

    Adding Logger

---
+++ src/main/java/ch/fhnw/digibp/classroom/adapter/LoggerDelegate.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.digibp.classroom.adapter;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Named;
+
+@Named("logger")
+public class LoggerDelegate {
+
+    private Logger logger = LoggerFactory.getLogger(LoggerDelegate.class);
+
+    public void execute(DelegateExecution execution) throws Exception {
+
+        logger.info("\n\n  ... LoggerDelegate invoked by "
+                + "processDefinitionId=" + execution.getProcessDefinitionId()
+                + ", tenantId=" + execution.getTenantId()
+                + ", activityId=" + execution.getCurrentActivityId()
+                + ", activityName='" + execution.getCurrentActivityName() + "'"
+                + ", processInstanceId=" + execution.getProcessInstanceId()
+                + ", businessKey=" + execution.getProcessBusinessKey()
+                + ", executionId=" + execution.getId()
+                + " \n\n");
+    }
+}
