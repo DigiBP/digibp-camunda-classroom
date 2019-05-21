099905381cf45cdc7c7534effa6a67bf3c6f0fdb
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-05-21 12:52:26 +0200

    Update

--- src/main/java/ch/fhnw/digibp/classroom/config/CamundaEngineConfig.java
+++ src/main/java/ch/fhnw/digibp/classroom/config/CamundaEngineConfig.java
@@ -8,14 +8,11 @@ package ch.fhnw.digibp.classroom.config;
 import ch.fhnw.digibp.classroom.tenant.TenantIdProviderPlugin;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
-import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.camunda.bpm.spring.boot.starter.configuration.Ordering;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 public class CamundaEngineConfig{
@@ -28,9 +25,5 @@ public class CamundaEngineConfig{
     public static ProcessEnginePlugin tenantIdConfiguration() {
         return new TenantIdProviderPlugin();
     }
-
-    @PostConstruct
-    public void cacheCapacity() {
-        //((SpringProcessEngineConfiguration) processEngineConfiguration).setCacheCapacity(0);
-    }
+    
 }
